0368d8a08f3c357f8476ff48bd8f5458ae88d77a
Marvellous Obatale <75810831+MARVELGARR@users.noreply.github.com>
2024-12-18 11:25:39 +0100

    added cors

--- index.ts
+++ index.ts
@@ -1,10 +1,20 @@
 import express, { Request, Response } from 'express';
 import router from './routes/routes';
+import cors from "cors"
 
 const app = express();
 const port = 3002;
 
 app.use(express.json());
+
+const corsOptions = {
+    origin: ['http://localhost:3000', 'https://your-domain.com'], // Allowed origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+    credentials: true, // Allow cookies and credentials
+};
+
+app.use(cors(corsOptions));
 
 // Use the property routes
 app.use("/api", router);
